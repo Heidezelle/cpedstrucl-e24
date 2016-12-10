89452c11846168fbc86620896eb3d5b4260ded11
Heidezelle L. Gadong <nicca966@gmail.com>
2016-12-10 10:57:24 +0800

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
-int main(){
+int main()
+{
 
 	int num[5]={1,4,3,4,4};
-	for(int i=0; i<5; i++){
+	for(int i=0; i<5; i++)
+	{
 		cout<<num[i]<<endl;
 	}
 	return 0;
