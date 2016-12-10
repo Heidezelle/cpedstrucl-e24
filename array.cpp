0a8017c928103f00f6426235ddb1494d5ef5b423
Heidezelle L. Gadong <nicca966@gmail.com>
2016-12-10 10:56:45 +0800

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-//1, 4, 3, 4, 4.
-	int ar[5]={1,4,3,4,4};
+
+	int num[5]={1,4,3,4,4};
 	for(int i=0; i<5; i++){
-		cout<<ar[i]<<endl;
+		cout<<num[i]<<endl;
 	}
 	return 0;
 }
